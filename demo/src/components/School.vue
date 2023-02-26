89097e7f1a30db079d87dda078503e4699b4e70e
liuzhipeng <liuzhipengdongjing@gmail.com>
2023-02-26 16:57:12 +0900

    day23

---
+++ demo/src/components/School.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 :style="{opacity}">这就是个傻逼demo</h1>
+        <h2>{{name}}</h2>
+        <h2>{{enName}}</h2>
+        <button @click="showname">点我提示名字</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name:'密山傻逼一中',
+            enName:'mishanshabiyizhong',
+            opacity:1
+        }
+    },
+    methods: {
+        showname(){
+            alert(this.name)
+        }   
+    },
+    mounted() {
+        console.log('mounted',this)
+				setInterval(() => {
+					this.opacity -= 0.01
+					if(this.opacity <= 0) this.opacity = 1
+				},16)
+    }
+}
+</script>
---
+++ demo/src/components/Student.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="demo">
+        <h2>{{stuName}}</h2>
+        <h2>{{stuEngName}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            stuName:'小红',
+            stuEngName:'xiaohong'
+        }
+    }
+}
+</script>
+
+<style>
+    .demo{
+        background-color: orange;
+    }
+</style>
