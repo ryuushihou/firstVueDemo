cea81599493a32c1d144cb294fe707795ef916ae
liuzhipeng <liuzhipengdongjing@gmail.com>
2023-03-04 22:31:10 +0900

    day25

--- demo/04_src_mixin混入(合)/components/Student.vue
+++ demo/04_src_mixin混入(合)/components/Student.vue
@@ -1,12 +1,13 @@
 <template>
 	<div>
-		<h2>学生姓名：{{name}}</h2>
+		<h2 @click="showName">学生姓名：{{name}}</h2>
 		<h2>学生性别：{{sex}}</h2>
-		<input type="text" v-fbind:value="name">
 	</div>
 </template>
 
 <script>
+	// import {hunhe,hunhe2} from '../mixin'
+
 	export default {
 		name:'Student',
 		data() {
@@ -15,5 +16,6 @@
 				sex:'男'
 			}
 		},
+		// mixins:[hunhe,hunhe2]
 	}
 </script>
